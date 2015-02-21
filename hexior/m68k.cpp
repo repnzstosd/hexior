0b772c4ea8ebc1d750e8d6cf93616c817e2a38c4
Peter Fors <peter.fors@mindkiller.com>
2015-02-21 16:30:07 +0100

    more code.

--- hexior/m68k.cpp
+++ hexior/m68k.cpp
@@ -60,9 +60,59 @@ int M68K::step() {
 	int instructionLength = 2;
 	uint16_t instruction = readWord(mPC);
 
+	switch(instruction >> 24) {
+		case 0x0:
+			// ORI, BTST, MOVEP, BCHG, BCLR, BSET, ANDI, SUBI, ADDI, EORI, CMPI
+
+		case 0x1:
+		case 0x2:
+		case 0x3:
+			// MOVE(A) .B/.W/.L
+
+		case 0x4:
+			// NEGX, MOVE from SR, CHK, LEA, CLR, NEG, MOVE to CCR, NOT, MOVE to SR, NBCD, PEA, SWAP, MOVEM, EXT, TST, TAS, TRAP, LINK, UNLK, MOVE USP, JSR, JMP
+
+		case 0x5:
+			// ADDQ, Scc, DBcc, SUBQ
+
+		case 0x6:
+			// Bcc, BRA, BSR
+
+		case 0x7:
+			// MOVEQ
+
+		case 0x8:
+			// OR, DIVU, SBCD, DIVS
+
+		case 0x9:
+			// SUB, SUBA, SUBX
+
+		case 0xa:
+			// nope...
+
+		case 0xb:
+			// CMP, CMPA, EOR, CMPM
+
+		case 0xc:
+			// AND, MULU, EXG, ABCD, MULS
+
+		case 0xd:
+			// ADD, ADDA, ADDX
+
+		case 0xe:
+			// ASd, LSd, ROXd, ROd
+
+		case 0xf:
+			// Fpu...
+	}
+
 	switch(instruction) {
 		case 0x003c:	// ORI to CCR
 		case 0x007c:	// ORI to SR
+		case 0x023c:	// ANDI to CCR
+		case 0x027c:	// ANDI to SR
+		case 0x0a2c:	// EORI to CCR
+		case 0x0a7c:	// EORI to SR
 		case 0x4afb:	// illegal
 		case 0x4e70:	// reset
 		case 0x4e71:	// nop
@@ -71,6 +121,7 @@ int M68K::step() {
 		case 0x4e75:	// rts
 		case 0x4e76:	// trapv
 		case 0x4e77:	// rtr
+			break;
 	}
 
 	switch(instruction & 0xffc0) {
@@ -84,6 +135,7 @@ int M68K::step() {
 		case 0x4ac:	// Tas
 		case 0x4e8:	// jsr
 		case 0x4ec:	// jmp
+			break;
 	}
 
 	switch(instruction & 0xf1c) {
@@ -93,6 +145,7 @@ int M68K::step() {
 		case 0x01a:	// BSET
 		case 0x41c:	// Lea
 		case 0x418:	// Chk
+			break;
 	}
 
 
